a7cdb063f3bb8f33c11e66ce65c673a8f5cccfcf
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-04-23 18:29:34 +0200

    update task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const API_URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = async name => {
-  const response = await fetch(`
-  ${API_URL}/${name}?fields=name,population,capital,flags,languages
-  `);
+  const response = await fetch(
+    `${API_URL}/${name}?fields=name,population,capital,flags,languages`
+  );
   return await response.json();
 };
